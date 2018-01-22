bd4dcc161af9ca339a23bac3a4510f3e8627222e
Alexandre Louisnard <alexandre.louisnard@avisto.com>
2018-01-22 11:04:04 +0100

    First commit for Mountains Finder AR app.

--- README.md
+++ README.md
@@ -1,41 +1,17 @@
-# Android Augmented Reality GPS Points
+# Android Mountains Finder Augmented Reality
 
 **Author: Alexandre Louisnard alouisnard@gmail.com**  
-**Android Java Library**  
-**2017** 
+**Android Java Application**  
+**2018** 
 
 ## DESCRIPTION
-Show points with GPS localization on the device camera preview.  
+Use the camera to identify the mountains and peaks around you with some augmented reality.
 
-**Functionalities:** 
-* Show markers with the name of the point, its altitude and its distance from the user on a camera preview background.
-* Import GPX files.
+Code reused from this library : https://github.com/AlexandreLouisnard/android-augmented-reality-gps-points
 
 ## USAGE
-**argps**: the library  
-**argpsapp** module: a sample application
 
 ### Library usage :
-**Gradle dependency**  
-
-* Add the following to your project level `build.gradle`:
-```gradle
-allprojects {
-	repositories {
-		maven { url "https://jitpack.io" }
-	}
-}
-```
-* Add this to your app `build.gradle`:
-```gradle
-dependencies {
-    compile 'com.github.AlexandreLouisnard:android-augmented-reality-gps-points:master-SNAPSHOT'
-}
-```
-
-You can extend AugmentedRealityActivity to get a simple example.
-
-See JAVADOC for the rest.
 
 ## CHANGELOG
 
---
+++ app/src/main/java/com/louisnard/mountainsfinderar/activities/AugmentedRealityActivity.java
@@ -0,0 +1,64 @@
+package com.louisnard.mountainsfinderar.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.louisnard.mountainsfinderar.R;
+import com.louisnard.mountainsfinderar.fragments.AugmentedRealityFragment;
+
+/**
+ * Augmented reality {@link AppCompatActivity} showing a {@link AugmentedRealityFragment}.<br>
+ *
+ * @author Alexandre Louisnard
+ */
+public class AugmentedRealityActivity extends AppCompatActivity {
+
+    // Tag
+    private static final String TAG = AugmentedRealityActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set layout
+        setContentView(R.layout.activity_toolbar_simple_fragment);
+
+        // Set toolbar
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set fragment
+        if (savedInstanceState == null) {
+            final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment, new AugmentedRealityFragment());
+            transaction.commit();
+        }
+    }
+
+    // Options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu_augmented_reality_activity, menu);
+        return true;
+    }
+
+    // Options menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            final Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
